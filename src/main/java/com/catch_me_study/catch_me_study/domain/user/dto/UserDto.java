84e66759c75fb1ddf72957ee3063de50de5d69b9
eunju0127 <aoddmswn0127@gmail.com>
2025-01-15 21:54:19 +0900

    UserDto 작성

--- src/main/java/com/catch_me_study/catch_me_study/domain/user/dto/UserDto.java
+++ src/main/java/com/catch_me_study/catch_me_study/domain/user/dto/UserDto.java
@@ -1,4 +1,33 @@
 package com.catch_me_study.catch_me_study.domain.user.dto;
 
-public class UserDto {
+import com.catch_me_study.catch_me_study.domain.common.BaseDto;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.validator.constraints.Length;
+import org.mapstruct.Mapper;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto extends BaseDto {
+
+    @NotNull
+    @Length(max = 255)
+    private String id;
+
+    @NotNull
+    @Length(max = 100)
+    private String email;
+
+    @NotNull
+    @Length(max = 80)
+    private String password;
+
+    @NotNull
+    @Length(max = 50)
+    private String name;
 }
